49755451ca68d6bf6b64cc51674bda49a0d8e8f4
wilfre-oss <63850794+wilfre-oss@users.noreply.github.com>
2022-05-04 21:54:44 +0200

    added tests

---
+++ test/se/kth/iv1350/saleSystem/controller/ControllerTest.java
@@ -0,0 +1,29 @@
+package se.kth.iv1350.saleSystem.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void startSale() {
+    }
+
+    @Test
+    void enterItem() {
+    }
+
+    @Test
+    void endSale() {
+    }
+
+    @Test
+    void enterAmountPaid() {
+    }
+}
---
+++ test/se/kth/iv1350/saleSystem/model/SaleTest.java
@@ -0,0 +1,49 @@
+package se.kth.iv1350.saleSystem.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import se.kth.iv1350.saleSystem.integration.ItemCatalog;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SaleTest {
+    private Sale sale;
+
+    @BeforeEach
+    void setUp() {
+        sale = new Sale();
+    }
+
+    @Test
+    void addItem() {
+
+    }
+
+    @Test
+    void endSale() {
+    }
+
+    @Test
+    void addPayment() {
+    }
+
+    @Test
+    void calculateTotalPrice() {
+    }
+
+    @Test
+    void getDateTime() {
+    }
+
+    @Test
+    void getTotalPrice() {
+    }
+
+    @Test
+    void getItemList() {
+    }
+
+    @Test
+    void getStore() {
+    }
+}
