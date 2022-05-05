25c4ea6f0fc0638ddca271a0f9b6f57c02d2c4b6
wilfre-oss <63850794+wilfre-oss@users.noreply.github.com>
2022-05-05 02:02:15 +0200

    tests

--- test/se/kth/iv1350/saleSystem/controller/ControllerTest.java
+++ test/se/kth/iv1350/saleSystem/controller/ControllerTest.java
@@ -4,37 +4,55 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import se.kth.iv1350.saleSystem.model.Sale;
 
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ControllerTest {
-    private Sale sale;
+    private Controller controller = new Controller();
 
 
     @BeforeEach
     void setUp() {
-        sale = new Sale();
-        sale.setTotalPrice(100);
-    }
-
-    @Test
-    void startSale() {
+        controller.startSale();
     }
 
     @Test
     void enterItem() {
+        Controller testController = controller;
+        controller.endSale();
+        if(testController.equals(controller))
+            fail("sale doesn't update in controller");
     }
 
     @Test
     void endSale() {
-
+        Controller testController = controller;
+        controller.endSale();
+        if(testController.equals(controller))
+            fail("sale doesn't update in controller");
     }
 
     @Test
     void enterAmountPaid() {
-        assertAll(() -> assertEquals(-20, sale.addPayment(80)),
-                () -> assertEquals(20, sale.addPayment(120)),
-              () -> assertEquals(100, sale.addPayment(200)),
-             () -> assertEquals(400, sale.addPayment(500)));
+        assertAll(() -> assertEquals(-20, controller.enterAmountPaid(80)),
+                () -> assertEquals(20, controller.enterAmountPaid(100)),
+              () -> assertEquals(100, controller.enterAmountPaid(200)),
+             () -> assertEquals(400, controller.enterAmountPaid(500)));
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof ControllerTest that))
+            return false;
+        return Objects.equals(controller, that.controller);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(controller);
     }
 }
--- test/se/kth/iv1350/saleSystem/model/SaleTest.java
+++ test/se/kth/iv1350/saleSystem/model/SaleTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import se.kth.iv1350.saleSystem.integration.ItemCatalog;
 
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SaleTest {
@@ -12,17 +14,22 @@ class SaleTest {
     @BeforeEach
     void setUp() {
         sale = new Sale();
-        sale.addItem(1);
-
     }
 
     @Test
     void addItem() {
-
+        Sale saleTest = sale;
+        sale.addItem(1);
+        if(saleTest.equals(sale))
+            fail("sale does not add item");
     }
 
     @Test
     void endSale() {
+        Sale saleTest = sale;
+        sale.addItem(1);
+        if(saleTest.equals(sale))
+            fail("sale doesn't update");
     }
 
     @Test
@@ -31,4 +38,17 @@ class SaleTest {
         assertEquals(80, sale.addPayment(100));
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof SaleTest saleTest))
+            return false;
+        return Objects.equals(sale, saleTest.sale);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sale);
+    }
 }
